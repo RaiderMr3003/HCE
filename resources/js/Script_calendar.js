19f795edddf6635a391ab1df25a22bd0449fe88e
Maximo Mercado <mr3003gean@gmail.com>
2025-07-14 17:05:29 -0500

    Se organizó el js y se inicio con la funcionalidad del modal

--- resources/js/Script_calendar.js
+++ resources/js/Script_calendar.js
@@ -2,64 +2,102 @@ import { Calendar } from '@fullcalendar/core'
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import esLocale from '@fullcalendar/core/locales/es';
+import { Modal } from 'bootstrap';
 
 import bootstrap5Plugin from '@fullcalendar/bootstrap5';
 
-document.addEventListener('DOMContentLoaded', function() {
-    const calendarEl = document.getElementById('calendar');
+document.addEventListener('DOMContentLoaded', function () {
+    let calendar;
+    
+    function init() {
+        const calendarEl = document.getElementById('calendar');
+        const modalCita = new Modal(document.getElementById('modal-cita'));
 
-    const calendar = new Calendar(calendarEl, {
-        plugins: [timeGridPlugin, interactionPlugin, bootstrap5Plugin ],
-        themeSystem: 'bootstrap5',
-        initialView: 'timeGridWeek',
-        headerToolbar: {
-            left: 'prev,today,next',
-            center: 'title',
-            right: 'timeGridDay,timeGridWeek'
-        },
-        allDaySlot: false,
-        locale: esLocale,
-        timeZone: 'local',
-        slotMinTime: "08:00:00",
-        slotMaxTime: "21:00:00",
-        slotDuration: "00:30:00",
-        slotLabelInterval: "00:30:00",
-        nowIndicator: true,
-        titleFormat: {
-            month: 'short',
-            day: '2-digit',
-            weekday: 'short'
-        },
-        slotLabelFormat: {
-            hour: '2-digit',
-            minute: '2-digit',
-            hour12: false
-        },
-        dayHeaderFormat: function(date) {
-            let localDate = new Date(date.date.marker);
-            localDate.setMinutes(localDate.getMinutes() + localDate.getTimezoneOffset());
-
-            return localDate.toLocaleDateString('es-ES', {
-                weekday: 'long',
+        calendar = new Calendar(calendarEl, {
+            plugins: [timeGridPlugin, interactionPlugin, bootstrap5Plugin],
+            themeSystem: 'bootstrap5',
+            initialView: 'timeGridWeek',
+            headerToolbar: {
+                left: 'prev,today,next',
+                center: 'title',
+                right: 'timeGridDay,timeGridWeek'
+            },
+            allDaySlot: false,
+            locale: esLocale,
+            timeZone: 'local',
+            slotMinTime: "08:00:00",
+            slotMaxTime: "21:00:00",
+            slotDuration: "00:30:00",
+            slotLabelInterval: "00:30:00",
+            nowIndicator: true,
+            titleFormat: {
+                month: 'short',
                 day: '2-digit',
-                month: '2-digit',
-                year: 'numeric',
-            }).toUpperCase();
-        },
-        selectable: true,
-        select: function(info) {
-            let nombrePaciente = prompt("Ingrese el nombre del paciente:");
-            if (nombrePaciente) {
-                calendar.addEvent({
-                    title: nombrePaciente,
-                    start: info.start,
-                    end: info.end,
-                    allDay: false
-                });
-            }
-            calendar.unselect();
-        },
-    });
+                weekday: 'short'
+            },
+            slotLabelFormat: {
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: false
+            },
+            dayHeaderFormat: formatearCabeceraDia,
+            selectable: true,
+            selectAllow: function(selectInfo) {
+                const duration = selectInfo.end - selectInfo.start;
+                return duration === 30 * 60 * 1000;
+            },
+            select: function(info) {
 
-    calendar.render();
+                const fechaCita = new Date(info.start).toLocaleDateString('es-PE', {
+                    day: '2-digit',
+                    month: '2-digit',
+                    year: 'numeric',
+                });
+
+                const horaInicio = new Date(info.start).toLocaleTimeString('es-PE', {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    hour12: false
+                });
+
+                const horaFin = new Date(info.end).toLocaleTimeString('es-PE', {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    hour12: false
+                });
+                document.getElementById('citaFecha').value = fechaCita;
+                document.getElementById('citaInicio').value = horaInicio;
+                document.getElementById('citaFin').value = horaFin;
+                modalCita.show();
+            }
+        });
+
+        calendar.render();
+        events();
+    }
+
+    function events() {
+        const modalElement = document.getElementById('modal-cita'); 
+        const formCita = document.getElementById('form-cita');
+        modalElement.addEventListener('hidden.bs.modal', function () {
+            formCita.reset();
+            document.getElementById('citainicio').value = '';
+            document.getElementById('citafin').value = '';
+        });
+    }
+
+    function formatearCabeceraDia(date) {
+        let localDate = new Date(date.date.marker);
+        localDate.setMinutes(localDate.getMinutes() + localDate.getTimezoneOffset());
+
+        return localDate.toLocaleDateString('es-ES', {
+            weekday: 'long',
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+        }).toUpperCase();
+    }
+
+    init();
+
 });
--- resources/views/components/calendar.blade.php
+++ resources/views/components/calendar.blade.php
@@ -9,39 +9,41 @@
 
 <div id="calendar"></div>
 
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+<div class="modal fade" id="modal-cita" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
     aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="staticBackdropLabel">Nueva Cita</h1>
+                <h1 class="modal-title fs-5">Nueva Cita</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form id="eventForm">
+                <form id="form-cita">
                     <div class="mb-3">
                         <label for="eventTitle" class="form-label">Buscar Paciente</label>
-                        <select name="txtPaciente" id="txtPaciente" class="form-select" id="txtPaciente" required>
+                        <select name="txtPaciente" id="txtPaciente" class="form-select" required>
                             <option value=""></option>
-                            <option value="NUEVO">(PACIENTE NUEVO)</option>
+                            <option value="0">(PACIENTE NUEVO)</option>
                         </select>
                     </div>
                     <div class="row align-items-center">
                         <div class="col mb-3">
-                            <label for="citainicio" class="form-label">Hora de inicio</label>
-                            <input type="text" class="form-control" id="citainicio" readonly>
+                            <label for="citaFecha" class="form-label">Fecha</label>
+                            <input type="text" class="form-control" id="citaFecha" disabled>
                         </div>
                         <div class="col mb-3">
-                            <label for="citafin" class="form-label">Hora de fin</label>
-                            <input type="text" class="form-control" id="citafin" readonly>
+                            <label for="citainicio" class="form-label">H. Inicio</label>
+                            <input type="text" class="form-control" id="citaInicio" disabled>
+                        </div>
+                        <div class="col mb-3">
+                            <label for="citafin" class="form-label">H. Fin</label>
+                            <input type="text" class="form-control" id="citaFin" disabled>
                         </div>
                     </div>
                     <div class="mb-3">
                         <label for="tpcita" class="form-label">Tipo de Cita</label>
-                        <select class="form-select" id="tpcita">
-                            <option value="Consulta">Consulta</option>
-                            <option value="Revisión">Revisión</option>
-                            <option value="Otro">Otro</option>
+                        <select class="form-select" id="tpCita">
+                            <option value=""></option>
                         </select>
                     </div>
                 </form>
